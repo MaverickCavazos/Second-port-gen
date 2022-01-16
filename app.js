d79c74f9c98fa22e574f5beba5d5cbd914f7dc7c
Maverick <Mav33987@gmail.com>
2022-01-16 00:12:56 -0600

    added inquirer

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
+/* const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name , github);
 
 
 
@@ -12,4 +21,4 @@ fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
 
     console.log('Porfolio complete! Check out index.html to see the output!')
-});
+}); */
